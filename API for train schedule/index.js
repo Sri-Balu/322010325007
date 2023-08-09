8e55ec33f3d045978de83490e180eeb62ac3459a
balaji <sribalaji1420@gmail.com>
2023-08-09 12:12:39 +0530

    Adjusted the code

--- API for train schedule/index.js
+++ API for train schedule/index.js
@@ -3,20 +3,18 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
-let cachedTrainData = []; // Initialize as an empty array
-const cacheDuration = 60000; // Cache data for 1 minute
+let cachedTrainData = []; 
+const cacheDuration = 60000;
 
-// Fetch train data from API and store in cache
 async function fetchAndCacheTrainData() {
     try {
         const response = await axios.get('URL_TO_John_Doe_Railways_API');
-        cachedTrainData = response.data.trains; // Assuming the API returns an object with a "trains" array
+        cachedTrainData = response.data.trains;
     } catch (error) {
         console.error(error);
     }
 }
 
-// Middleware to refresh cached data
 async function refreshCachedData() {
     if ((Date.now() - lastFetchTime) > cacheDuration) {
         await fetchAndCacheTrainData();
@@ -24,23 +22,19 @@ async function refreshCachedData() {
     }
 }
 
-// Route to get sorted and filtered train information
 app.get('/trains', async (req, res) => {
     await refreshCachedData();
 
     try {
-        // Perform sorting and filtering on cachedTrainData
         const sortedAndFilteredTrains = cachedTrainData
             .filter(train => {
-                // Apply your filtering logic here
                 return true;
             })
             .sort((a, b) => {
-                // Implement sorting logic based on price, tickets, departure time
-                return 0; // Your sorting logic here
+               
+                return 0; 
             });
 
-        // Transform the data into the desired response format
         const transformedTrains = sortedAndFilteredTrains.map(train => ({
             name: train.name,
             departureTime: train.departureTime,
